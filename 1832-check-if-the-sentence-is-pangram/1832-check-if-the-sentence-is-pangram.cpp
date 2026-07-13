fc1a4d8f923dd399d58f3f1c49f2940fba7ad1d4
Sujay Kumar Singh <sujaykrsingh999@gmail.com>
2026-07-13 05:35:33 +0530

    Time: 2 ms (14.01%), Space: 8.8 MB (63.66%) - LeetHub

---
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+ 
+       
+       // array approch 
+     int arr[26]={0} ;
+
+     for(int i = 0 ; i < sentence.size() ; i++){
+        arr[sentence[i]-'a']++ ; 
+       
+     }
+       for(int i = 0 ; i < 26 ; i++){
+
+       if(arr[i]==0) return false ; 
+        
+    }
+     return true ;
+    }
+};
