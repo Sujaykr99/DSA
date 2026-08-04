392b9a799f7cfc775cf255cd33719b8709d7760d
Sujay Kumar Singh <sujaykrsingh999@gmail.com>
2026-08-05 04:12:21 +0530

    Time: 0 ms (100%), Space: 8.8 MB (97.99%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,10 +1,26 @@
 class Solution {
+    private:
+    bool solve(int n ){
+
+        //base case 
+        if(n==1) return true ; 
+
+        if(n<=0||n%3!=0) return false ;
+        
+        return solve(n/3) ;
+
+    }
 public:
     bool isPowerOfThree(int n) {
-        for(int i = 0 ; i < 20 ; i++){
-            long long ans = pow(3,i) ;
-            if(ans == n) return true ;
-        }
-        return false ;
+        //BY loop 
+        // for(int i = 0 ; i < 20 ; i++){
+        //     long long ans = pow(3,i) ;
+        //     if(ans == n) return true ;
+        // }
+        // return false ;
+
+        //by recusion
+        bool ans = solve(n) ;
+       return ans ;
     }
 };
