1d3cfa21dba8155dd10159124839adadf1c2f453
Sujay Kumar Singh <sujaykrsingh999@gmail.com>
2026-08-09 11:37:42 +0530

    Time: 0 ms (100%), Space: 10 MB (27.28%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -8,31 +8,65 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+// int getlength(ListNode * head){
+//     int len = 0 ;
+//     while(head !=NULL){
+//         len++ ;
+//         head = head->next ;
+//     }
+//     return len ;
+// }
+//     ListNode* middleNode(ListNode* head) {
+
+//         int len = getlength(head) ;
+
+//         int ans = len/2 ;
+
+//         ListNode * temp = head ;
+
+//         int count=0 ; 
+
+//         while(count<ans){
+
+//             temp = temp->next ;
+//             count++ ;
+//         }
+//         return temp ;
+//     }
+// };
+
+//approch 2
+
 class Solution {
 public:
-int getlength(ListNode * head){
-    int len = 0 ;
-    while(head !=NULL){
-        len++ ;
-        head = head->next ;
-    }
-    return len ;
+
+ListNode * getmiddle(ListNode * head){
+
+    if(head==NULL || head->next == NULL) return head ;
+
+
+    if(head->next->next==NULL) return head->next ;
+     
+     ListNode * fast = head->next ;
+     ListNode * slow = head ;
+
+     while(fast!=NULL){
+        fast = fast->next ; 
+
+        if(fast!=NULL){
+            fast = fast -> next ;
+
+        }
+        slow = slow->next ;
+     }
+     return slow ;
+    
 }
     ListNode* middleNode(ListNode* head) {
-
-        int len = getlength(head) ;
-
-        int ans = len/2 ;
-
-        ListNode * temp = head ;
-
-        int count=0 ; 
-
-        while(count<ans){
-
-            temp = temp->next ;
-            count++ ;
-        }
-        return temp ;
+     
+     return getmiddle(head) ;
+       
     }
 };
