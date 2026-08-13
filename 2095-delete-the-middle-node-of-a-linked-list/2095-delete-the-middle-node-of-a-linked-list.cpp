9aad1f919ed23dcf3a67cd34722273036dba1fbe
Sujay Kumar Singh <sujaykrsingh999@gmail.com>
2026-08-13 12:25:18 +0530

    Time: 4 ms (35.78%), Space: 312 MB (54.21%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -15,8 +15,9 @@ private:
         ListNode* fast = head;
 
         while (fast != NULL && fast->next != NULL) {
-            slow = slow->next;
             fast = fast->next->next;
+            slow = slow->next;
+            
         }
 
         return slow;
