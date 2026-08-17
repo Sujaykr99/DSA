ffebbd72286bb24791da0a12d0c63459f05c0705
Sujay Kumar Singh <sujaykrsingh999@gmail.com>
2026-08-18 00:15:13 +0530

    Time: 1 ms (83.79%), Space: 28.7 MB (22.15%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,30 +1,62 @@
+// class Solution {
+// public:
+//     vector<int> nextGreaterElements(vector<int>& nums) {
+
+//         vector<int>ans ;
+
+//         for(int i = 0 ; i < nums.size() ; i++){
+
+//             bool found = false ;
+
+//               for(int j = 1 ; j < nums.size() ; j++){
+
+//                 int index = (i + j) % nums.size() ;
+
+//                    if(nums[index]>nums[i]){
+
+//                     ans.push_back(nums[index]) ;
+//                     found = true ;
+//                     break ;
+//                    }
+//               }
+
+//             if(found== false){
+//                 ans.push_back(-1) ;
+//             }
+//         }
+//     return ans ;
+
+//     }
+// };
+
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
+        //  2n iterations
+        int n = nums.size();
+        vector<int> ans(n, -1);
+        stack<int> st;
 
-        vector<int>ans ;
+        for (int i = 2 * n - 1; i >= 0; i--) {
 
-        for(int i = 0 ; i < nums.size() ; i++){
-            
-            bool found = false ;
-            
-              for(int j = 1 ; j < nums.size() ; j++){
+            int index = i % n;
 
-                int index = (i + j) % nums.size() ;
+            while (!st.empty() && st.top() <= nums[index]) {
 
-                   if(nums[index]>nums[i]){
-
-                    ans.push_back(nums[index]) ;
-                    found = true ;
-                    break ;
-                   }
-              }
-
-            if(found== false){
-                ans.push_back(-1) ;
+                st.pop();
             }
+            if (i < n) {
+                if (st.empty()) {
+                    ans[index] = -1;
+
+                } else {
+                    ans[index] = st.top();
+                }
+            }
+
+            st.push(nums[index]);
         }
-    return ans ;
-        
+
+        return ans;
     }
 };
