c553dc734ba25ef6166a02782b69adfdc83816cc
Sujay Kumar Singh <sujaykrsingh999@gmail.com>
2026-08-17 17:42:38 +0530

    Time: 0 ms (100%), Space: 12.8 MB (40.18%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,33 +1,74 @@
+
+// // brute - force version
+// class Solution {
+// public:
+//     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+
+//         vector<int> ans;
+
+//         for (int i = 0; i < nums1.size(); i++) {
+
+//             for (int j = 0; j < nums2.size(); j++) {
+
+//                 if (nums1[i] == nums2[j]) {
+
+//                     int found = false;
+
+//                     for (int k = j + 1; k < nums2.size(); k++) {
+
+//                         if (nums2[k] > nums1[i]) {
+
+//                             ans.push_back(nums2[k]);
+//                             found = true;
+//                             break;
+//                         }
+//                     }
+//                     if (found == false) {
+//                         ans.push_back(-1);
+//                     }
+//                 }
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
 
-        vector<int> ans;
+        unordered_map<int, int> mpp;
 
-        for (int i = 0; i < nums1.size(); i++) {
+        stack<int> st;
 
-            for (int j = 0; j < nums2.size(); j++) {
+        vector<int> ans(nums2.size());
+        for (int i = nums2.size() - 1; i >= 0; i--) {
 
-                if (nums1[i] == nums2[j]) {
+            while (!st.empty() && st.top() <= nums2[i]) {
 
-                    int found = false;
-
-                    for (int k = j+1; k < nums2.size(); k++) {
-
-                        if (nums2[k] > nums1[i]) {
-
-                            ans.push_back(nums2[k]);
-                            found = true;
-                            break;
-                        }
-                    }
-                    if (found == false) {
-                        ans.push_back(-1);
-                    }
-                }
+                st.pop();
             }
+
+            if (st.empty()) {
+                ans[i] = -1;
+            } else {
+                ans[i] = st.top();
+            }
+
+            st.push(nums2[i]);
         }
 
-        return ans;
+        for (int i = 0; i < nums2.size(); i++) {
+
+            mpp[nums2[i]] = ans[i];
+        }
+
+        vector<int> greater;
+
+        for (int i = 0; i < nums1.size(); i++) {
+            greater.push_back(mpp[nums1[i]]) ;
+        }
+        return greater;
     }
 };
