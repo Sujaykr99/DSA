b75b931a2697625924a6121c559dea6828acd943
Sujay Kumar Singh <sujaykrsingh999@gmail.com>
2026-09-08 19:55:17 +0530

    Time: 0 ms (100%), Space: 23.5 MB (93.81%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -9,7 +9,9 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class Solution {
+
+
+ /* class Solution {
 public:
     int maxDepth(TreeNode* root) {
 
@@ -38,5 +40,37 @@ public:
          int ans = max(opt1 , max(opt2 , opt3)) ;
 
          return ans ;
+    }
+}; tc - O(n2) */
+               
+class Solution {
+public:
+    pair<int,int>diameterfast(TreeNode* root){
+
+        if(root == NULL){
+            pair<int,int> p = make_pair(0,0) ;
+            return p ;
+        }
+        pair<int,int>left = diameterfast(root->left) ;
+         pair<int,int>right = diameterfast(root->right) ;
+
+         int opt1 = left.first ;
+         int opt2 = right.first ;
+         int opt3 = left.second + right.second + 1 ;
+
+         pair<int,int>ans ;
+         ans.first = max(opt1 , max(opt2 , opt3)) ;
+         ans.second = max(left.second , right.second) +1 ;
+        
+         return ans ;
+        
+
+    }
+
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+
+        return diameterfast(root).first - 1 ;
+        
     }
 };
